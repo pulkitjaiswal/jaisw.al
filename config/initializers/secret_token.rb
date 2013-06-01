3c364a4baabcaafaa8abb959d3a36b887c62dc9d
Pulkit Jaiswal <pulkitj@icloud.com>
2013-05-31 23:05:43 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JaiswAl::Application.config.secret_token = '93ff5b60367ff8b6d521fe6646f5308f14dfd36f9e0c18c21199c852ebdabd5750188e8b77c3f6812612699d1e0a6cb5ffbce71daba3550cd39424cef26cf3d4'
